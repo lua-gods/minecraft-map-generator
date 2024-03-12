4277c10c047c3afa87c8fe514c04914af7ef8114
auriafoxgirl <53555592+auriafoxgirl@users.noreply.github.com>
2024-03-12 17:03:32 +0100

    faster horizontal lines

--- main.lua
+++ main.lua
@@ -3,15 +3,14 @@ local flatColors, staircaseColors = require('color_blocks')
 -- variables
 local startHeight = 80
 local generated = {}
-local height = 0
 local currentPixel = -1
 
 local staircase = true
+local stairs = {[-1] = {}, [1] = {}}
 
-local generate = false
 local commandSpeed = 1
-local mapGenerateSpeed = 100000
-local currentCommand = 0
+local commands = {}
+local commandData = {}
 
 local tick = 0
 
@@ -47,40 +46,43 @@ end
 
 local function generateMap()
    currentPixel = 0
-   generate = false
    generated = {}
+   for x = 0, 127 do
+      generated[x] = {x = x, type = 'line'}
+   end
 end
 generateMap()
 
 local function buildMap()
-   if not areaPos or generate then return end
-   generate = true
-   currentCommand = 0
-   if #generated == 0 then
-      generateMap()
+   if not areaPos then return end
+   table.insert(commands, {type = 'command', '/fill '..areaPos.x..' '..startHeight..' '..(areaPos.y - 1)..' '..(areaPos.x + 127)..' '..startHeight..' '..(areaPos.y - 1)..' stone'})
+   for _, v in pairs(generated) do
+      table.insert(commands, v)
    end
-   host:sendChatCommand('/fill '..areaPos.x..' '..startHeight..' '..(areaPos.y - 1)..' '..(areaPos.x + 127)..' '..startHeight..' '..(areaPos.y - 1)..' stone')
 end
+
 -- render
 function events.world_render()
    if currentPixel == -1 then return end
    local colors = staircase and staircaseColors or flatColors
-   while avatar:getCurrentInstructions() < mapGenerateSpeed and currentPixel < 128 * 128 do
+   local maxTime = client.getSystemTime() + 10
+   while client.getSystemTime() < maxTime and currentPixel < 128 * 128 do
       local x, y = math.floor(currentPixel / 128), currentPixel % 128
-      if y == 0 then height = startHeight table.insert(generated, {x = x}) end
+      -- if y == 0 then table.insert(generated, {x = x, type = 'line'}) end
       local orginalColor = orginal:getPixel(x, y).xyz
-      local dist = 100
-      local best
+      local dist = 10
+      local best--, best2
       for _, v in pairs(colors) do
          local newDist = (v[1] - orginalColor):lengthSquared()
          if newDist < dist then
             dist = newDist
+            -- best2 = best
             best = v
          end
       end
+      -- if (x + y) % 2 == 0 then best = best2 end
       preview:setPixel(x, y,best[1])
-      height = height + best[3]
-      table.insert(generated[#generated], {best[2], height})
+      table.insert(generated[x], {best[2], best[3]})
       currentPixel = currentPixel + 1
    end
    preview:update()
@@ -89,21 +91,43 @@ function events.world_render()
    end
 end
 -- generate
+local commandTypes = {
+   line = function(tbl)
+      local data = commandData
+      data.current = (data.current or 0) + 1
+      if data.current > #tbl then return true end
+      local blockData = tbl[data.current]
+      if blockData[2] == 0 then
+         local block = blockData[1]
+         local start = data.current - 1
+         for _ = data.current, #tbl - 1 do
+            local nextBlockData = tbl[data.current + 1]
+            if nextBlockData[1] ~= block or nextBlockData[2] ~= 0 then break end
+            data.current = data.current + 1
+         end
+         local x = areaPos.x + tbl.x
+         host:sendChatCommand('/fill '..x..' '..data.height..' '..(areaPos.y + start)..' '..x..' '..data.height..' '..(areaPos.y + data.current - 1)..' '..blockData[1])
+      else
+         data.height = (data.height or startHeight) + blockData[2]
+         host:sendChatCommand('/setblock '..(areaPos.x + tbl.x)..' '..data.height..' '..(areaPos.y + data.current - 1)..' '..blockData[1])
+      end
+   end,
+   command = function(tbl)
+      host:sendChatCommand(tbl[1])
+      return true
+   end
+}
 function events.tick()
-   if not generate or currentPixel ~= -1 then return end
-   if #generated == 0 then print('done') generate = false return end
+   -- if not generate or currentPixel ~= -1 then return end
+   if #commands == 0 then return end
    tick = (tick + 1) % 20
    local commandCount = math.floor(tick / 20 * commandSpeed) - math.floor((tick - 1) / 20 * commandSpeed)
    for _ = 1, commandCount do
-      local commands = generated[1]
-      currentCommand = currentCommand + 1
-      if currentCommand > #commands then
-         table.remove(generated, 1)
-         currentCommand = 0
-         if #generated == 0 then return end
-      else
-         local command = commands[currentCommand]
-         host:sendChatCommand('/setblock '..(areaPos.x + commands.x)..' '..command[2]..' '..(areaPos.y + currentCommand - 1)..' '..command[1])
+      local finished = commandTypes[commands[1].type](commands[1])
+      if finished then
+         table.remove(commands, 1)
+         commandData = {}
+         if #commands == 0 then return end
       end
    end
 end
@@ -115,11 +139,10 @@ page:newText():setText('Select area'):onPress(setMapPos)
 page:newText():setText('Clear area'):onPress(function()
    if not areaPos then return end
    local min, max = world.getBuildHeight()
-   host:sendChatCommand('//pos1 '..table.concat({areaPos.x_y:add(0, min, 0):unpack()}, ','))
-   host:sendChatCommand('//pos2 '..table.concat({areaPos.x_y:add(127, max, 127):unpack()}, ','))
-   host:sendChatCommand('//set air')
+   table.insert(commands, {type = 'command', '//pos1 '..table.concat({areaPos.x_y:add(0, min, 0):unpack()}, ',')})
+   table.insert(commands, {type = 'command', '//pos2 '..table.concat({areaPos.x_y:add(127, max, 127):unpack()}, ',')})
+   table.insert(commands, {type = 'command', '//set air'})
 end):setMargin(10)
 page:newToggle():setText('Staircase method'):onToggle(function(toggled) staircase = toggled generateMap() end):setToggled(staircase):setMargin(10)
 page:newSlider():setText('cmd/s'):setRange(1, 1024):setStep(16, 1):setValue(commandSpeed):onScroll(function(value) commandSpeed = value end)
-page:newSlider():setText('px/tick'):setRange(1, 1000000):setStep(1000, 1):setValue(mapGenerateSpeed):onScroll(function(value) mapGenerateSpeed = value end)
 page:newText():setText('generate'):onPress(buildMap)
